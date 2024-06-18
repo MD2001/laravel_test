5240a2baca72157df6e091aecb202e7e52ca76bb
Mohamed Diaa <mdiea0581@gmail.com>
2024-06-18 11:12:06 +0300

    aynamc the style of page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>about</title>
-</head>
-<body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">about</a>
-        <a href="/contact">contact</a>
-    </nav>
+<x-layout>
+    <x-slot:heading>
+        About page
+    </x-slot:heading>
     <h1> welcome to about page.</h1>
-</body>
-</html>
+</x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        home page
+    </x-slot:heading>
     <h1> welcome to home page.</h1>
 </x-layout>
