a82fb7e02f9b3efa9b30a19fe63a368d6f3bd113
Mohamed Diaa <mdiea0581@gmail.com>
2024-06-20 12:06:04 +0300

    eloquent v0.2

--- app/Models/job.php
+++ app/Models/job.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class job extends Model
 {
+    use HasFactory;
    protected $table="job_listing";
    protected $fillable=["title","salary"];
 }
--- database/migrations/2024_06_20_085729_create_employees_table.php
+++ database/migrations/2024_06_20_085729_create_employees_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listing', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\Employee::class);
-            $table->string('title');
-            $table->integer('salary');
+            $table->string("name");
             $table->timestamps();
         });
     }
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listing');
+        Schema::dropIfExists('employees');
     }
 };
