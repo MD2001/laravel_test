f709e6df9321efe9ad6aa8a2fe82e012b3962559
Mohamed Diaa <mdiea0581@gmail.com>
2024-06-22 11:11:19 +0300

    migratiation problem

--- database/factories/CareerFactory.php
+++ database/factories/CareerFactory.php
@@ -6,9 +6,9 @@ use App\Models\Employee;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\job>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\career>
  */
-class jobFactory extends Factory
+class CareerFactory extends Factory
 {
     /**
      * Define the model's default state.
